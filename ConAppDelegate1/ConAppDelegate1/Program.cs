69dc481a6e50b8b67fa5ef7d0b1f5744b9e6f393
Akrishna521 <anishetty.saikrishna@mphasis.com>
2023-11-27 15:52:13 +0530

    27-11-2023

---
+++ ConAppDelegate1/ConAppDelegate1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ArithmeticOperations
+{
+    public delegate int ArithmeticOperation(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithmeticOperation operation;
+            Console.WriteLine("Enter the arithmetic operation you want to perform:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    operation = Add;
+                    break;
+                case 2:
+                    operation = Subtract;
+                    break;
+                case 3:
+                    operation = Multiply;
+                    break;
+                case 4:
+                    operation = Divide;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    return;
+            }
+            Console.WriteLine("Enter the first number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int result = operation(x, y);
+            Console.WriteLine("The result is: {0}", result);
+
+            Console.ReadKey();
+        }
+        private static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        private static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        private static int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+
+            return x / y;
+        }
+    }
+}
---
+++ ConAppDelegate2/ConAppDelegate2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public delegate T Operation<T>(T a, T b);
+
+public static class Operations
+{
+    public static T Add<T>(T a, T b)
+    {
+        return (dynamic)a + (dynamic)b;
+    }
+
+    public static T Subtract<T>(T a, T b)
+    {
+        return (dynamic)a - (dynamic)b;
+    }
+
+    public static T Multiply<T>(T a, T b)
+    {
+        return (dynamic)a * (dynamic)b;
+    }
+
+    public static T Divide<T>(T a, T b)
+    {
+        return (dynamic)a / (dynamic)b;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Operation<int> add = Operations.Add<int>;
+        Operation<int> subtract = Operations.Subtract<int>;
+        Operation<int> multiply = Operations.Multiply<int>;
+        Operation<int> divide = Operations.Divide<int>;
+        Console.WriteLine("Enter two integers: ");
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Choose an operation: ");
+        Console.WriteLine("1. Add");
+        Console.WriteLine("2. Subtract");
+        Console.WriteLine("3. Multiply");
+        Console.WriteLine("4. Divide");
+
+        int operation = int.Parse(Console.ReadLine());
+        int result;
+        switch (operation)
+        {
+            case 1:
+                result = add(a, b);
+                break;
+            case 2:
+                result = subtract(a, b);
+                break;
+            case 3:
+                result = multiply(a, b);
+                break;
+            case 4:
+                result = divide(a, b);
+                break;
+            default:
+                throw new Exception("Invalid operation");
+        }
+        Console.WriteLine("The result is: {0}", result);
+    }
+}
